ca9abc4eec18724e5d66dff40dcee5d3eb52422d
NthPortal <nthportal@gmail.com>
2017-04-05 13:22:18 -0400

    Finalize SecureErase

--- src/main/java/tmp/namespace/undecided/SecureErase.java
+++ src/main/java/tmp/namespace/undecided/SecureErase.java
@@ -4,28 +4,29 @@ import com.google.common.base.Preconditions;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import tmp.namespace.undecided.specs.AdvancedFileDeletionSpec;
-import tmp.namespace.undecided.specs.AdvancedFileDeletionSpec.Conf;
 
 import java.io.IOException;
 
+/**
+ * Static utility for securely erasing files.
+ */
 public final class SecureErase {
-    public static void eraseFile(String fileName, FileErasureSpecProvider provider) throws IOException {
-        eraseFile(fileName, provider.get());
-    }
+    private SecureErase() {}
 
-    public static void eraseFile(String fileName, FileErasureSpec erasureSpec) throws IOException {
-        if (erasureSpec.isTerminal()) {
-            doEraseFile(fileName, erasureSpec);
-        } else {
-            doEraseFile(fileName, erasureSpec.andThen(new AdvancedFileDeletionSpec(Conf.newBuilder().result())));
-        }
-    }
-
-    private static void doEraseFile(String fileName, FileErasureSpec erasureSpec) throws IllegalArgumentException, IOException {
-        Path path = new Path(fileName);
+    /**
+     * Erases a file on HDFS.
+     *
+     * @param filePath    the string path to the file
+     * @param erasureSpec the {@link FileErasureSpec} with which to erase the file
+     * @throws IllegalArgumentException if the path does not exist or does not refer
+     *                                  to a regular file
+     * @throws IOException              if an I/O error occurs while erasing the file
+     */
+    public static void eraseFile(String filePath, FileErasureSpec erasureSpec) throws IllegalArgumentException, IOException {
+        Path path = new Path(filePath);
         try (FileSystem fs = FileSystem.get(new Configuration())) {
             // Check that path is a regular file
+            Preconditions.checkArgument(fs.exists(path), "File does not exist: " + path);
             Preconditions.checkArgument(fs.getFileStatus(path).isFile(), "Path is not a regular file: " + path);
 
             erasureSpec.eraseFile(fs, path);
